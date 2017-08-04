482577c41ac907acbadcbc506e75d521a9fe6fdb
Santiago Saavedra <ssaavedra@openshine.com>
2017-08-04 14:41:45 +0200

    Take version info from pbr on __init__

--- es_query_builder/__init__.py
+++ es_query_builder/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from pbr.version import VersionInfo
+_v = VersionInfo(__name__).semantic_version()
 
 __author__ = """Santiago Saavedra"""
 __email__ = 'ssaavedra@openshine.com'
-__version__ = '0.1.0'
+
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
